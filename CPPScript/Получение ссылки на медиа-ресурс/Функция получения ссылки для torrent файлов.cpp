1202f8a0fa34cfb8f5edf67a30ae3e6dcb68e8f7
WendyH <Wendy.Hane@gmail.com>
2015-11-07 15:51:35 +0600

    + функция ссылки для torrent файлов

---
+++ CPPScript/Получение ссылки на медиа-ресурс/Функция получения ссылки для torrent файлов.cpp
@@ -0,0 +1,11 @@
+﻿// ---- Формирование ссылки для просмотра torrent файла -----------------------
+void GetTorrentMediaResourceLink(String sFile, int nIndex = 1) {
+  String sDownloadsDir, sVal, sLink;
+
+  sDownloadsDir = HmsTranscodingTempDirectory + "Torrents";
+  sLink = Format('torrent:%s?index=%d&savepath=%s', [sFile, nIndex, sDownloadsDir]);
+  if (HmsRegExMatch('--portbegin=(\\d+)', mpPodcastParameters, sVal)) sLink += '&portbegin='+sVal;
+  if (HmsRegExMatch('--portend=(\\d+)'  , mpPodcastParameters, sVal)) sLink += '&portend='  +sVal;
+  if (HmsRegExMatch('--sslport=(\\d+)'  , mpPodcastParameters, sVal)) sLink += '&sslport='  +sVal;
+  MediaResourceLink = sLink;        
+}
---
+++ PascalScript/Получение ссылки на медиа-ресурс/Функция получения ссылки для torrent файлов.pas
@@ -0,0 +1,12 @@
+﻿// ---- Формирование ссылки для просмотра torrent файла -----------------------
+Procedure GetTorrentMediaResourceLink(sFile: String; nIndex: Integer = 1);
+Var
+  sDownloadsDir, sVal, sLink: String;
+Begin
+  sDownloadsDir := HmsTranscodingTempDirectory + "Torrents";
+  sLink := Format('torrent:%s?index=%d&savepath=%s', [sFile, nIndex, sDownloadsDir]);
+  if HmsRegExMatch('--portbegin=(\\d+)', mpPodcastParameters, sVal) Then sLink := sLink+'&portbegin='+sVal;
+  if HmsRegExMatch('--portend=(\\d+)'  , mpPodcastParameters, sVal) Then sLink := sLink+'&portend='  +sVal;
+  if HmsRegExMatch('--sslport=(\\d+)'  , mpPodcastParameters, sVal) Then sLink := sLink+'&sslport='  +sVal;
+  MediaResourceLink := sLink;        
+End;
