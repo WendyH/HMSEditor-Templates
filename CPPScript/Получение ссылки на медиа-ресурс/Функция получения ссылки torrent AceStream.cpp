c4d260820b3c88510170366696134b59f0ba4424
WendyH <Wendy.Hane@gmail.com>
2015-11-07 14:52:58 +0600

    + функции ссылки torrent AceStream

---
+++ CPPScript/Получение ссылки на медиа-ресурс/Функция получения ссылки torrent AceStream.cpp
@@ -0,0 +1,7 @@
+﻿// ---- Получение ссылки на ресурс из torrent файла через AceStream -----------
+void GetLink_AceStreamTorrent(String sFile) {
+  String sCmd, sPlayer;
+  sCmd    = 'cmd://"%s" --no-crashdump --play-and-exit --language en -Idummy --demuxdump-file="<OUTPUT FILE>" vlc://pause:4 --access=p2p_access "%s" :demux=dump';
+  sPlayer = ExtractShortPathName(RegistryRead('Software\\AceStream\\InstallDir'))+'\\Player\\ace_player.exe';
+  MediaResourceLink = Format(sCmd, [sPlayer, sFile]);
+}
---
+++ PascalScript/Получение ссылки на медиа-ресурс/Функция получения ссылки torrent AceStream.pas
@@ -0,0 +1,9 @@
+﻿// ---- Получение ссылки на ресурс из torrent файла через AceStream -----------
+Procedure GetLink_AceStreamTorrent(sFile: String);
+Var
+  sCmd, sPlayer: String;
+Begin
+  sCmd    := 'cmd://"%s" --no-crashdump --play-and-exit --language en -Idummy --demuxdump-file="<OUTPUT FILE>" vlc://pause:4 --access=p2p_access "%s" :demux=dump';
+  sPlayer := ExtractShortPathName(RegistryRead('Software\AceStream\InstallDir'))+'\Player\ace_player.exe';
+  MediaResourceLink := Format(sCmd, [sPlayer, sFile]);
+End;
