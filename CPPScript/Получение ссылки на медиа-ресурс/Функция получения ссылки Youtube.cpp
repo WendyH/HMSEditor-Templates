fa2577c351b1196ba9f8a2a12ccef8223263cc8a
WendyH <Wendy.Hane@gmail.com>
2015-12-20 23:57:14 +0600

    Зменена функция получения ссылки для youtube

--- CPPScript/Получение ссылки на медиа-ресурс/Функция получения ссылки Youtube.cpp
+++ CPPScript/Получение ссылки на медиа-ресурс/Функция получения ссылки Youtube.cpp
@@ -1,47 +1,133 @@
-﻿// ------------------------------------------- Получение ссылки на Youtube ----
-void GetLink_Youtube3(string sLink) {
-  string 
-    sData, sVideoID='', sMaxHeight='', sAudio='', 
-    sSubtitlesLanguage='ru', sSubtitlesUrl, sFile, sVal, sMsg; 
-  TJsonObject JSON; 
+﻿// ---- Получение ссылки на Youtube -------------------------------------------
+bool GetLink_Youtube31(string sLink) {
+  String sData, sVideoID='', sMaxHeight='', sSubtitlesLanguage='ru';
+  String hlsUrl, ttsUrl, flp, jsUrl, dashMpdLink, streamMap, playerId, algorithm;
+  String sType, itag, sig, alg, s, sFile, sVal, sConfig, sHeaders;
+  String UrlBase = ""; bool is3D;
+  int  i, n, w, num, height, priority, minPriority = 90, selHeight, maxHeight = 1080;
+  TJsonObject JSON; TRegExpr RegEx;
 
+  sHeaders = 'Referer: '+sLink+#13#10+
+             'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'+#13#10+
+             'Origin: http://www.youtube.com'+#13#10;
+  
   HmsRegExMatch('--maxheight=(\\d+)'    , mpPodcastParameters, sMaxHeight);
   HmsRegExMatch('--sublanguage=(\\w{2})', mpPodcastParameters, sSubtitlesLanguage);
   bool bSubtitles = (Pos('--subtitles'  , mpPodcastParameters)>0);  
   bool bAdaptive  = (Pos('--adaptive'   , mpPodcastParameters)>0);  
-  bool bNotDE     = (Pos('notde=1'      , sLink)>0);  
+  TryStrToInt(sMaxHeight, maxHeight);
 
   if (!HmsRegExMatch('[\\?&]v=([^&]+)'       , sLink, sVideoID))
        HmsRegExMatch('/(?:embed|v)/([^\\?]+)', sLink, sVideoID);
-
-  if (sVideoID=='') { HmsLogMessage(1, 'Невозможно получить Video ID в ссылке Youtube'); return; }
-
-  sLink = 'http://hms.lostcut.net/youtube/g.php?v='+sVideoID;
-  if (sMaxHeight!=''                  ) sLink += '&max_height='+sMaxHeight;
-  if (Trim(mpPodcastMediaFormats )!='') sLink += '&media_formats='+mpPodcastMediaFormats;
-//if (Trim(goRoot[mpiAccessToken])!='') sLink += '&auth='+goRoot[mpiAccessToken];
-  if (bAdaptive                       ) sLink += '&adaptive=1';
-  if (bNotDE                          ) sLink += '&notde=1';
-    
-  sData = HmsUtf8Decode(HmsDownloadUrl(sLink));
-  // if error - exists reason
-  if (HmsRegExMatch('"reason":"(.*?)"' , sData, sMsg)) { HmsLogMessage(2 , sMsg); return; }
-
-  JSON  = TJsonObject.Create(); 
-  try {
-    JSON.LoadFromString(sData);
-    MediaResourceLink = JSON.S['url'   ];
-    sSubtitlesUrl     = JSON.S['ttsUrl'];
-    sAudio            = JSON.S['audio' ];
-    if (sAudio!='') MediaResourceLink = Format('-i "%s" -i "%s"', [MediaResourceLink, sAudio]); 
-  } finally { JSON.Free(); }
   
-  if (Pos('m3u8', MediaResourceLink)>0) MediaResourceLink = ' '+Trim(MediaResourceLink);
+  if (sVideoID=='') {
+    sData = HmsDownloadURL(sLink, sHeaders, True);
+    if (!HmsRegExMatch('youtube.com[^"\'>]+v=([^&]+)', sData, sVideoID))
+         HmsRegExMatch('/embed/([^\\?]+)'            , sData, sVideoID);
+  }
 
-  // Если есть субтитры и в дополнительных параметрах еуказано их показывать - загружаем 
-  if (bSubtitles && (sSubtitlesUrl!='')) {
+  if (sVideoID=='') return VideoMessage('Невозможно получить Video ID в ссылке Youtube');
+
+  sLink = 'http://www.youtube.com/watch?v='+sVideoID+'&hl=ru&persist_hl=1&has_verified=1';
+  
+  sData = HmsUtf8Decode(HmsDownloadURL(sLink));
+  sData = HmsRemoveLineBreaks(sData);
+  HmsRegExMatch('player.config\\s*?=\\s*?({.*?});', sData, sConfig);
+  
+  JSON = TJsonObject.Create();
+  try {
+    JSON.LoadFromString(sConfig);
+    hlsUrl      = HmsExpandLink(JSON.S['args\\hlsvp' ], UrlBase);
+    ttsUrl      = HmsExpandLink(JSON.S['args\\ttsurl'], UrlBase);
+    flp         = HmsExpandLink(JSON.S['url'         ], UrlBase);
+    jsUrl       = HmsExpandLink(JSON.S['assets\\js'  ], UrlBase);
+    streamMap   = JSON.S['args\\url_encoded_fmt_stream_map'];
+    if (bAdaptive && JSON.B['args\\adaptive_fmts']) 
+      streamMap = JSON.S['args\\adaptive_fmts'];
+    if ((streamMap=='') && (hlsUrl=='')) {
+      HmsLogMessage(2, "Can not found stream map in player config");
+      return;
+    }
+  } finally { JSON.Free; }
+  if (Copy(jsUrl, 1, 2)=='//') jsUrl = 'http:'+Trim(jsUrl);
+  HmsRegExMatch('/player-([\\w_-]+)/', jsUrl, playerId);
+  algorithm = HmsDownloadURL('https://hms.lostcut.net/youtube/getalgo.php?jsurl='+HmsHttpEncode(jsUrl));
+  
+  if (hlsUrl!='') {
+    MediaResourceLink = ' '+hlsUrl;
+    sData = HmsDownloadUrl(sLink, sHeaders, true);
+    RegEx = TRegExpr.Create('BANDWIDTH=(\\d+).*?RESOLUTION=(\\d+)x(\\d+).*?(http[^#]*)', PCRE_SINGLELINE);
+    try {
+      if (RegEx.Search(sData)) do {
+        sLink = ' ' + RegEx.Match(4);
+        height = StrToIntDef(RegEx.Match(3), 0);
+        if (mpPodcastMediaFormats!='') {
+          priority = HmsMediaFormatPriority(height, mpPodcastMediaFormats);
+          if ((priority>=0) && (priority>minPriority)) {
+            MediaResourceLink = sLink; minPriority = priority;
+          }
+        } else if ((height > selHeight) && (height <= maxHeight)) {
+          MediaResourceLink = sLink; selHeight = height;
+        }
+      } while (RegEx.SearchAgain());
+    } finally { RegEx.Free(); }
+  
+  } else if (streamMap!='') {
+      i=1; while (i<=Length(streamMap)) {
+        sData = Trim(ExtractStr(streamMap, ',', i));
+        sType = HmsHttpDecode(ExtractParam(sData, 'type', '', '&'));
+        itag  = ExtractParam(sData, 'itag'    , '', '&');
+        is3D  = ExtractParam(sData, 'stereo3d', '', '&') == '1';
+        sLink = '';
+        if (Pos('url=', sData)>0) {
+          sLink = ' ' + HmsHttpDecode(ExtractParam(sData, 'url', '', '&'));
+          if (Pos('&signature=', sLink)==0) {
+            sig = HmsHttpDecode(ExtractParam(sData, 'sig', '', '&'));    
+            if (sig=='') {
+              sig = HmsHttpDecode(ExtractParam(sData, 's', '', '&'));
+              for (w=1; w<=WordCount(algorithm, ' '); w++) {
+                alg = ExtractWord(w, algorithm, ' ');
+                if (Length(alg)<1) continue;
+                if (Length(alg)>1) TryStrToInt(Copy(alg, 2, 4), num);
+                if (alg[1]=='r') {s=''; for(n=Length(sig); n>0; n--) s+=sig[n]; sig = s;   } // Reverse
+                if (alg[1]=='s') {sig = Copy(sig, num+1, Length(sig));                     } // Clone
+                if (alg[1]=='w') {n = (num-Trunc(num/Length(sig)))+1; Swap(sig[1], sig[n]);} // Swap
+              }
+            }
+            if (sig!='') sLink += '&signature=' + sig;
+          }
+        }
+        if (sLink!='') {
+          if ((Pos('flv', sType)>0) || (Pos('mp4', sType)>0)) {
+            height = 0;
+            if      (itag in ([13,17,160,36           ])) height = 144;
+            else if (itag in ([5,83,133,242           ])) height = 240;
+            else if (itag in ([6                      ])) height = 270;
+            else if (itag in ([18,34,43,82,100,134,243])) height = 360;
+            else if (itag in ([35,44,101,135,244,43   ])) height = 480;
+            else if (itag in ([22,45,84,102,136,247   ])) height = 720;
+            else if (itag in ([37,46,137,248          ])) height = 1080;
+            else if (itag in ([264,271                ])) height = 1440;
+            else if (itag in ([266                    ])) height = 2160;
+            else if (itag in ([138,272                ])) height = 2304;
+            else if (itag in ([38                     ])) height = 3072;
+            if (mpPodcastMediaFormats!='') {
+              priority = HmsMediaFormatPriority(height, mpPodcastMediaFormats);
+              if ((priority>=0) || (priority<minPriority)) {
+                MediaResourceLink = sLink; minPriority = priority; selHeight = height;
+              }
+            } else if ((height>selHeight) && (height<= maxHeight)) {
+              MediaResourceLink = sLink; selHeight = height;
+            }
+          }
+        }
+      }    
+    
+  }
+  // Если есть субтитры и в дополнительных параметрах указано их показывать - загружаем 
+  if (bSubtitles && (ttsUrl!='')) {
     sFile = HmsSubtitlesDirectory+'\\Youtube\\'+PodcastItem.ItemID+'.'+sSubtitlesLanguage+'.srt';
-    sLink = sSubtitlesUrl+'&format=srt&lang='; 
+    sLink = ttsUrl+'&format=srt&lang='; 
     if (!HmsDownloadURLToFile(sLink+sSubtitlesLanguage, sFile, 'Accept-Encoding: gzip, deflate')) {
       HmsDownloadURLToFile(sLink+'en'                 , sFile, 'Accept-Encoding: gzip, deflate');
     }
--- PascalScript/Получение ссылки на медиа-ресурс/Функция получения ссылки Youtube.pas
+++ PascalScript/Получение ссылки на медиа-ресурс/Функция получения ссылки Youtube.pas
@@ -1,51 +1,142 @@
 ﻿// ---- Получение ссылки на Youtube -------------------------------------------
-Procedure GetLink_Youtube3(sLink: String);
+Function GetLink_Youtube31(sLink: String): Boolean;
 Var
-  sData, sVideoID, sMaxHeight, sAudio, sSubtitlesLanguage, sSubtitlesUrl, sFile, sVal, sMsg: String; 
-  JSON: TJsonObject; bSubtitles, bAdaptive, bNotDE: Boolean;
-Begin
-  sSubtitlesLanguage := 'ru';
-  HmsRegExMatch('--maxheight=(\\d+)'    , mpPodcastParameters, sMaxHeight);
-  HmsRegExMatch('--sublanguage=(\\w{2})', mpPodcastParameters, sSubtitlesLanguage);
+  sData, sVideoID, sMaxHeight, sSubtitlesLanguage:String;
+  hlsUrl, ttsUrl, flp, jsUrl, dashMpdLink, streamMap, playerId, algorithm: String;
+  sType, itag, sig, alg, s, sFile, sVal, sConfig, sHeaders: String ;
+  UrlBase:String = ""; is3D, bSubtitles, bAdaptive: Boolean;
+  i, n, w, num, height, priority, minPriority, selHeight, maxHeight: Integer;
+  JSON: TJsonObject; RegEx: TRegExpr;
+Begin 
+  sSubtitlesLanguage :='ru'; minPriority := 90; maxHeight := 1080;
+  sHeaders := 'Referer: '+sLink+#13#10+
+              'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36'+#13#10+
+              'Origin: http://www.youtube.com'+#13#10;
+  
+  HmsRegExMatch('--maxheight=(\d+)'    , mpPodcastParameters, sMaxHeight);
+  HmsRegExMatch('--sublanguage=(\w{2})', mpPodcastParameters, sSubtitlesLanguage);
   bSubtitles := (Pos('--subtitles'  , mpPodcastParameters)>0);  
   bAdaptive  := (Pos('--adaptive'   , mpPodcastParameters)>0);  
-  bNotDE     := (Pos('notde=1'      , sLink)>0);  
+  TryStrToInt(sMaxHeight, maxHeight);
 
-  If Not HmsRegExMatch('[\\?&]v=([^&]+)'       , sLink, sVideoID) Then
-         HmsRegExMatch('/(?:embed|v)/([^\\?]+)', sLink, sVideoID);
+  If Not HmsRegExMatch('[\?&]v=([^&]+)'       , sLink, sVideoID) Then
+         HmsRegExMatch('/(?:embed|v)/([^\?]+)', sLink, sVideoID);
+  
+  If sVideoID='' Then Begin
+    sData := HmsDownloadURL(sLink, sHeaders, True);
+    If Not HmsRegExMatch('youtube.com[^"''>]+v=([^&]+)', sData, sVideoID) Then
+           HmsRegExMatch('/embed/([^\?]+)'             , sData, sVideoID);
+  End;
 
-  If sVideoID='' Then Begin HmsLogMessage(1, 'Невозможно получить Video ID в ссылке Youtube'); Exit; End;
+  If sVideoID='' Then Begin HmsLogMessage(2, 'Невозможно получить Video ID в ссылке Youtube'); Exit; End;
 
-  sLink := 'http://hms.lostcut.net/youtube/g.php?v='+sVideoID;
-  If (sMaxHeight<>''                  ) Then sLink := sLink+'&max_height='+sMaxHeight;
-  If (Trim(mpPodcastMediaFormats )<>'') Then sLink := sLink+'&media_formats='+mpPodcastMediaFormats;
-//If (Trim(goRoot[mpiAccessToken])<>'') Then sLink := sLink+'&auth='+goRoot[mpiAccessToken];
-  If (bAdaptive                       ) Then sLink := sLink+'&adaptive=1';
-  If (bNotDE                          ) Then sLink := sLink+'&notde=1';
-    
-  sData := HmsUtf8Decode(HmsDownloadUrl(sLink));
-  // if error - exists reason
-  If HmsRegExMatch('"reason":"(.*?)"' , sData, sMsg) Then Begin HmsLogMessage(2 , sMsg); Exit; End;
-
-  JSON := TJsonObject.Create(); 
-  Try 
-    JSON.LoadFromString(sData);
-    MediaResourceLink := JSON.S['url'   ];
-    sSubtitlesUrl     := JSON.S['ttsUrl'];
-    sAudio            := JSON.S['audio' ];
-    If sAudio<>'' Then MediaResourceLink := Format('-i "%s" -i "%s"', [MediaResourceLink, sAudio]); 
+  sLink := 'http://www.youtube.com/watch?v='+sVideoID+'&hl=ru&persist_hl=1&has_verified=1';
+  
+  sData := HmsUtf8Decode(HmsDownloadURL(sLink));
+  sData := HmsRemoveLineBreaks(sData);
+  HmsRegExMatch('player.config\s*?=\s*?({.*?});', sData, sConfig);
+  
+  JSON := TJsonObject.Create();
+  Try
+    JSON.LoadFromString(sConfig);
+    hlsUrl      := HmsExpandLink(JSON.S['args\hlsvp' ], UrlBase);
+    ttsUrl      := HmsExpandLink(JSON.S['args\ttsurl'], UrlBase);
+    flp         := HmsExpandLink(JSON.S['url'        ], UrlBase);
+    jsUrl       := HmsExpandLink(JSON.S['assets\js'  ], UrlBase);
+    streamMap   := JSON.S['args\url_encoded_fmt_stream_map'];
+    If bAdaptive AND JSON.B['args\adaptive_fmts'] Then 
+      streamMap := JSON.S['args\adaptive_fmts'];
+    If (streamMap='') AND (hlsUrl='') Then Begin
+      HmsLogMessage(2, "Can not found stream map in player config");
+      Exit;
+    End;
   Finally 
     JSON.Free;
   End;
+  If Copy(jsUrl, 1, 2)='//' Then jsUrl := 'http:'+Trim(jsUrl);
+  HmsRegExMatch('/player-([\w_-]+)/', jsUrl, playerId);
+  algorithm := HmsDownloadURL('https://hms.lostcut.net/youtube/getalgo.php?jsurl='+HmsHttpEncode(jsUrl));
   
-  If Pos('m3u8', MediaResourceLink)>0 Then MediaResourceLink := ' '+Trim(MediaResourceLink);
-
-  // Если есть субтитры и в дополнительных параметрах еуказано их показывать - загружаем 
-  If bSubtitles AND (sSubtitlesUrl<>'') Then Begin
+  If hlsUrl<>'' Then Begin
+    MediaResourceLink := ' '+hlsUrl;
+    sData := HmsDownloadUrl(sLink, sHeaders, true);
+    RegEx := TRegExpr.Create('BANDWIDTH=(\d+).*?RESOLUTION=(\d+)x(\d+).*?(http[^#]*)', PCRE_SINGLELINE);
+    try 
+      If RegEx.Search(sData) Then Repeat
+        sLink  := ' ' + RegEx.Match(4);
+        height := StrToIntDef(RegEx.Match(3), 0);
+        If mpPodcastMediaFormats<>'' Then Begin
+          priority := HmsMediaFormatPriority(height, mpPodcastMediaFormats);
+          If (priority>=0) AND (priority>minPriority) Then Begin
+            MediaResourceLink := sLink; minPriority := priority;
+          End;
+        End Else If ((height > selHeight) AND (height <= maxHeight)) Then Begin
+          MediaResourceLink := sLink; selHeight := height;
+        End;
+      Until Not RegEx.SearchAgain();
+    Finally 
+      RegEx.Free();
+    End;
+  
+  End Else If streamMap<>'' Then Begin
+    i:=1; While (i<=Length(streamMap)) Do 
+    Begin
+      sData := Trim(ExtractStr(streamMap, ',', i));
+      sType := HmsHttpDecode(ExtractParam(sData, 'type', '', '&'));
+      itag  := ExtractParam(sData, 'itag'    , '', '&');
+      is3D  := ExtractParam(sData, 'stereo3d', '', '&') = '1';
+      sLink := '';
+      If (Pos('url=', sData)>0) Then Begin
+        sLink := ' ' + HmsHttpDecode(ExtractParam(sData, 'url', '', '&'));
+        If (Pos('&signature=', sLink)=0) Then Begin
+          sig := HmsHttpDecode(ExtractParam(sData, 'sig', '', '&'));    
+          If (sig='') Then Begin
+            sig := HmsHttpDecode(ExtractParam(sData, 's', '', '&'));
+            For w:=1 To WordCount(algorithm, ' ') Do Begin
+              alg := ExtractWord(w, algorithm, ' ');
+              If (Length(alg)<1) Then continue;
+              If (Length(alg)>1) Then TryStrToInt(Copy(alg, 2, 4), num);
+              If (alg[1]='r') Then Begin s:=''; for n:=Length(sig) DownTo 1 Do s:=s+sig[n]; sig := s; End; // Reverse
+              If (alg[1]='s') Then Begin sig := Copy(sig, num+1, Length(sig));                        End; // Clone
+              If (alg[1]='w') Then Begin n := (num-Trunc(num/Length(sig)))+1; Swap(sig[1], sig[n]);   End; // Swap
+            End;
+          End;
+          If sig<>'' Then sLink := sLink + '&signature=' + sig;
+        End;
+      End;
+      If (sLink<>'') Then Begin
+        If ((Pos('flv', sType)>0) Or (Pos('mp4', sType)>0)) Then Begin
+          height := 0;
+          If      (itag in ([13,17,160,36           ])) Then height := 144
+          Else if (itag in ([5,83,133,242           ])) Then height := 240
+          Else if (itag in ([6                      ])) Then height := 270
+          Else if (itag in ([18,34,43,82,100,134,243])) Then height := 360
+          Else if (itag in ([35,44,101,135,244,43   ])) Then height := 480
+          Else if (itag in ([22,45,84,102,136,247   ])) Then height := 720
+          Else if (itag in ([37,46,137,248          ])) Then height := 1080
+          Else if (itag in ([264,271                ])) Then height := 1440
+          Else if (itag in ([266                    ])) Then height := 2160
+          Else if (itag in ([138,272                ])) Then height := 2304
+          Else if (itag in ([38                     ])) Then height := 3072;
+          If (mpPodcastMediaFormats<>'') Then Begin
+            priority := HmsMediaFormatPriority(height, mpPodcastMediaFormats);
+            If ((priority>=0) Or (priority<minPriority)) Then Begin
+              MediaResourceLink := sLink; minPriority := priority; selHeight := height;
+            End;
+          End Else If ((height>selHeight) And (height<= maxHeight)) Then Begin
+            MediaResourceLink := sLink; selHeight := height;
+          End;
+        End;
+      End;
+    End;
+    
+  End;
+  // Если есть субтитры и в дополнительных параметрах указано их показывать - загружаем 
+  If (bSubtitles AND (ttsUrl<>'')) Then Begin
     sFile := HmsSubtitlesDirectory+'\Youtube\'+PodcastItem.ItemID+'.'+sSubtitlesLanguage+'.srt';
-    sLink := sSubtitlesUrl+'&format=srt&lang='; 
+    sLink := ttsUrl+'&format=srt&lang='; 
     If Not HmsDownloadURLToFile(sLink+sSubtitlesLanguage, sFile, 'Accept-Encoding: gzip, deflate') Then
            HmsDownloadURLToFile(sLink+'en'              , sFile, 'Accept-Encoding: gzip, deflate');
-    PodcastItem.Properties[mpiSubtitleLanguage] := sFile;
+    PodcastItem[mpiSubtitleLanguage] := sFile;
   End;
 End;
